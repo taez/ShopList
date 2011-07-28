4478891fadffbc1a9dde78707e08c30ad6a52d6a
taez Kim <kaoskim@gmail.com>
2011-07-28 21:49:53 +0900

    New PJ

---
+++ ShopList/ShopList/ShopListAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShopListAppDelegate.h
+//  ShopList
+//
+//  Created by 태제 김 on 11. 7. 28..
+//  Copyright 2011년 taez77@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopListAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ShopList/ShopList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShopList
+//
+//  Created by 태제 김 on 11. 7. 28..
+//  Copyright 2011년 taez77@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
