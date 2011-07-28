97804300947707d8c0ca64e22c01e73099d3e419
taez Kim <kaoskim@gmail.com>
2011-07-28 22:04:58 +0900

    add

---
+++ ShopList/ShopList/Classes/AppMainController.h
@@ -0,0 +1,13 @@
+//
+//  AppMainController.h
+//  ShopList
+//
+//  Created by 태제 김 on 11. 7. 28..
+//  Copyright 2011년 taez77@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppMainController : UIViewController
+
+@end
