f388f857875d9794aed971bd99fae7aa99842f25
Youngtaek Oh <coanyaa@gmail.com>
2011-08-04 22:12:18 +0900

    coanyaa test view controller added

---
+++ ShopList/ShopList/Classes/coanyaatestviewcontroller.h
@@ -0,0 +1,13 @@
+//
+//  coanyaatestviewcontroller.h
+//  ShopList
+//
+//  Created by YoungTaeck Oh on 11. 8. 4..
+//  Copyright 2011년 taez77@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface coanyaatestviewcontroller : UIViewController
+
+@end
